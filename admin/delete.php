735b0d17c4fc5d5617377caa51e724c05d39c5c4
yashD17 <73748641+yashD17@users.noreply.github.com>
2020-11-05 22:28:24 +0400

    Add files via upload

---
+++ admin/delete.php
@@ -0,0 +1,20 @@
+<?php 
+$connection = mysqli_connect("localhost","root","");
+$db = mysqli_select_db($connection, 'admin');
+
+if(isset($_POST['deletedata']))
+{
+	$id = mysqli_escape_string($connection,$_POST['delete_id']);
+
+	$query = "DELETE FROM staff WHERE id='$id' ";
+	$query_run = mysqli_query($connection, $query);
+
+	if($query_run){
+		echo '<script> alert("Data Deleted"); </script>';
+		header("Location:addstaff.php");
+	}else{
+		echo '<script> alert("Data Not Deleted"); </script>';
+	}
+}
+
+?>
